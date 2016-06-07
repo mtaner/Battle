a830dc8f3c2556318aafe878066a57625bad7b22
Marlon Date-Chong <mdatechong@gmail.com>
2016-06-07 15:13:12 +0100

    added names to battle

---
+++ spec/app_spec.rb
@@ -0,0 +1,13 @@
+require 'sinatra'
+require 'spec_helper'
+
+
+feature 'Enter names' do
+  it "shows names on webpage" do
+  visit('/')
+  fill_in(:player_1_name, with: "Marlon")
+  fill_in(:player_2_name, with: "Merve")
+  click_button 'Submit'
+  expect(page).to have_content('Marlon vs. Merve')
+  end
+end
