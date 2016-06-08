4f7f3f1c9969cdb7c5e198a52d1613913fddece0
mtaner <mervetaner88@gmail.com>
2016-06-08 12:18:57 +0100

    Created a battle folder, gemfile, specfile and added the a form for player names

---
+++ spec/battle_app_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+
+feature 'Forms' do
+	scenario "users can add their names to the form" do
+		visit("/")
+		fill_in :player_1_name, with: "Merveron"
+		fill_in :player_2_name, with: "Camerve"
+		click_button "Play"
+		expect(page).to have_content "Merveron vs. Camerve"
+	end
+end
