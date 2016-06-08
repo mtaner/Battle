517939246327986e71298c390423d4e29a53b251
Cameron Epstein <cameron.a.epstein@gmail.com>
2016-06-08 15:38:18 +0100

    Intro to the web: 20_viewing_hit_points Complete

--- spec/feature/battle_app_spec.rb
+++ spec/feature/battle_app_spec.rb
@@ -9,4 +9,13 @@ feature 'Forms' do
 		click_button "Play"
 		expect(page).to have_content "Merveron vs. Camerve"
 	end
+
+	scenario "Users can see eachothers hit points" do
+			visit("/")
+		fill_in :player_1_name, with: "Merveron"
+		fill_in :player_2_name, with: "Camerve"
+		click_button "Play"
+		expect(page).to have_content "Camerve HP is: 1"
+	end 
+
 end
